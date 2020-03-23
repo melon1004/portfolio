e3d6bd533f92dd97428ffb0cd7e6e75fcbaa7ac3
melon1004 <rjdnf6565@gmail.com>
2020-03-23 15:11:01 +0900

    아이유 첫 커밋

---
+++ 포트폴리오/iu/index.js
@@ -0,0 +1,70 @@
+function ActiveOnVisible__init() {
+    $(window).resize(function() {
+        setTimeout(function() {
+            ActiveOnVisible__initOffset();
+        }, 500);
+    });
+    ActiveOnVisible__initOffset();
+
+    $(window).scroll(ActiveOnVisible__checkAndActive);
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__initOffset() {
+    $('.active-on-visible').each(function(index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.offset().top;
+        $node.attr('data-active-on-visible-offsetTop', offsetTop);
+        
+        if ( !$node.attr('data-active-on-visible-diff-y') ) {
+            $node.attr('data-active-on-visible-diff-y', '0');
+        }
+        
+        if ( !$node.attr('data-active-on-visible-delay') ) {
+            $node.attr('data-active-on-visible-delay', '0');
+        }
+    });
+    
+    ActiveOnVisible__checkAndActive();
+}
+
+function ActiveOnVisible__checkAndActive() { 
+    $('.active-on-visible:not(.active)').each(function(index, node) {
+        var $node = $(node);
+
+        var offsetTop = $node.attr('data-active-on-visible-offsetTop') * 1;
+        var diffY = parseInt($node.attr('data-active-on-visible-diff-y'));
+        var delay = parseInt($node.attr('data-active-on-visible-delay'));
+        
+        if ( $(window).scrollTop() + $(window).height() + diffY > offsetTop ) {
+            setTimeout(function() {
+                $node.addClass('active');
+            }, delay);
+        }
+    });
+}
+
+ActiveOnVisible__init();
+
+
+
+
+
+//스크롤
+
+
+
+
+console.clear();
+
+$(window).scroll(function() {
+    var scrollTop = $(window).scrollTop();
+    
+    if ( scrollTop >= 200 ) {
+        $('html').addClass('scroll-top-over-200');
+    }
+    else {
+        $('html').removeClass('scroll-top-over-200');
+    }
+});
